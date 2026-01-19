d77b8459c0cd67ee24dced2707c58644d836cd79
Ben Joe Mathews <joemathews4development@gmail.com>
2026-01-19 17:07:27 +0100

    Completed the tasks for Day 1

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,52 @@
 class Quiz {
     // YOUR CODE HERE:
     //
+
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
 
     // 2. getQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex]
+    }
     
     // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex += 1
+    }
 
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;
+        }
+        /*for (const question of this.questions) {
+            question.shuffleChoices()
+        }*/
+        /*this.questions.forEach(element => {
+            element.shuffleQuestions()
+        });*/
+    }
 
     // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        if (this.questions[this.currentQuestionIndex].answer === answer) {
+            this.correctAnswers += 1
+        }
+    }
 
     // 6. hasEnded()
+    hasEnded() {
+        return this.currentQuestionIndex >=  this.questions.length
+    }
+
 }
